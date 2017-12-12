0a870ca09a09babfb05905ed6b18d31f0ee187a5
Abradat <shayankhorsandi1376@gmail.com>
2017-12-12 15:49:35 +0330

    bug fixes

--- nachos-3.4/code/prepfile.bash
+++ nachos-3.4/code/prepfile.bash
@@ -3,5 +3,5 @@ export CSE120_COMMON_CFLAGS="-m32"
 export CSE120_COMMON_ASFLAGS="--32"
 export CSE120_LDFLAGS="-m32"
 export CSE120_CFLAGS="-m32 -I./ -I../threads -g"
-export CSE120_GCC=/home/slytanix/Documents/Nachos_Operating_Systems_Course/mips-x86.linux-xgcc/
+export CSE120_GCC=/home/slytanix/Documents/test/Nachos_Operating_Systems_Course/mips-x86.linux-xgcc/
 export MAKE_CMD="make"
