89a28f00daa9d36f0cf2ef0f253d734cd9c52bf4
Abradat <shayankhorsandi1376@gmail.com>
2017-12-12 16:37:57 +0330

    thread.h modified to pq

--- nachos-3.4/code/threads/thread.h
+++ nachos-3.4/code/threads/thread.h
@@ -81,6 +81,8 @@ class Thread {
     int machineState[MachineStateSize];  // all registers except for stackTop
 
   public:
+
+    int priority = -1;
     Thread(char* debugName);		// initialize a Thread 
     ~Thread(); 				// deallocate a Thread
 					// NOTE -- thread being deleted
