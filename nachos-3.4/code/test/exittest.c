c15d3179981c45463861434e96d541f231b09b90
ahmadreza1376 <sezavar.1997@gmail.com>
2018-01-02 03:21:48 -0800

    Add exittest.c

---
+++ nachos-3.4/code/test/exittest.c
@@ -0,0 +1,19 @@
+//
+// Created by ahmadreza on 1/2/18.
+//
+
+/*
+ * exittest.c
+ *
+ * Simple program to test exit system call.
+ */
+
+#include "syscall.h"
+
+int
+main()
+{
+    Exit(123);
+}
+
+
